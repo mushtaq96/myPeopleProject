a203b3f784dc9a4de53fe1e78133bde0f4777eb3
mushtaq96 <mushtaq96smb@gmail.com>
2021-01-23 23:25:13 +0900

    unstable verison commit only for reference

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { Button, Linking, StyleSheet, Text, View } from 'react-native';
 import RNLocation from 'react-native-location';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
 
 RNLocation.configure({
   distanceFilter: 0//configure the distance to default value in meters
 })
 
-const App = () => {
+const App = ({ url }) => {
 
-  [viewLocation, isViewLocation] = useState([])
+  const [viewLocation, isViewLocation] = useState([
+    viewLocation.longitude = 0,
+    viewLocation.latitude = 0
+  ]);
 
   const [tweet, setTweet] = useState([viewLocation.longitude, viewLocation.latitude]);
 
@@ -28,7 +33,7 @@ const App = () => {
   }
 
   //requesting to access location data
-  const permissionHandle = async () => {//this will return a promise
+  const getLocation = async () => {//this will return a promise
     console.log('i am here')
     //check existing permission before requesting
     let permission = await RNLocation.checkPermission({
@@ -55,31 +60,43 @@ const App = () => {
       })
       console.log(permission)
       userLocation = await RNLocation.getLatestLocation({ timeout: 100 })
-      console.log(userLocation, userLocation.longitude, userLocation.latitude, userLocation.timestamp)
+      console.log(userLocation)
       isViewLocation(userLocation)
     }
     else {
       console.log('here again')
       userLocation = await RNLocation.getLatestLocation({ timeout: 100 })
-      console.log(userLocation, userLocation.longitude, userLocation.latitude, userLocation.timestamp)
+      console.log(userLocation)
       isViewLocation(userLocation)
       setTweet([viewLocation.longitude, viewLocation.latitude])
     }
   }
+
   return (
     <View style={styles.container}>
       <Text>Welcome to my People's app!</Text>
-
       <View style={{ marginTop: 10, padding: 10 }}>
         <Button title="get location"
-          onPress={permissionHandle} />
+          onPress={getLocation} />
       </View>
       <Text>Latitude:{viewLocation.latitude} </Text>
       <Text>Longitude:{viewLocation.longitude}</Text>
       <View style={{ marginTop: 10, padding: 10 }}>
-        <Button title="send location" onPress={tweetLocation} />
+        <Button title="send location to my twitter feed" onPress={tweetLocation} />
       </View>
-    </View>
+      <MapView
+        style={{ width: '100%', height: 400 }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={{
+          latitude: { viewLocation.latitude },
+          longitude: viewLocation.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      ></MapView>
+    </View >
+
   );
 };
 
